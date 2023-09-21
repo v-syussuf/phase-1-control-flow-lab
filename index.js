23d107d04c853bd8438e3e8c13000b073825f853
Yussuf <yussuf.saumu@gmail.com>
2023-09-21 14:34:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return ('This one is on me!');
+  }
+  else if (distance >= 2000 && distance < 2500){
+    return ('I will gladly take your thirty bucks.');
+  }
+  else if(distance > 2500){
+    return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination == "NYC"? 'Ok, sounds good.'
+  : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch(tip){
+  case 'generous':
+    console.log('Thank you so much.');
+    break;
+  case 'not as generous':
+    console.log('Thank you.');
+    break;
+  default:
+    console.log('Bye.');
+}
 }
